f045d753b55821c5d29772adbb8f0271f2b1bde5
Jar <thatrandomjar@users.noreply.github.com>
2023-09-25 04:35:23 +0530

    set up a webapp

---
+++ app.js
@@ -0,0 +1,25 @@
+const ul_1 = document.querySelector(".option1");
+const ul_2 = document.querySelector(".option2");
+const ul_3 = document.querySelector(".option3");
+const ul_4 = document.querySelector(".option4");
+const ul_5 = document.querySelector(".option5");
+const ul_6 = document.querySelector(".option6");
+const ul_7 = document.querySelector(".option7");
+const ul_8 = document.querySelector(".option8");
+const ul_9 = document.querySelector(".option9");
+const ul_10 = document.querySelector(".option10");
+const ul_11 =document.querySelector(".option11");
+const ul_12 = document.querySelector(".option12");
+const ul_13 = document.querySelector(".option13");
+const ul_14 = document.querySelector(".option14");
+const ul_15 = document.querySelector(".option15");
+const ul_16 = document.querySelector(".option16");
+const ul_17 = document.querySelector(".option17");
+
+const q1 = document.querySelector(".q1")
+
+
+//first question
+ul_1.addEventListener("click", function(){
+    q1.style.display = "none";
+})
